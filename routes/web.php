3fe6b46c813f25bc6aa1ca14bcdf85b6bb7ef7a9
cahyadi-bs <cahbayu27@gmail.com>
2021-09-08 20:46:55 +0700

    #1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Cahyadi",
+        "email" => "cahyadi@gmail.com",
+        "img" => "xiao.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
 });
