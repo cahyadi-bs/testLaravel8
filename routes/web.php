e8beeee8f6dc4bd2b95e00f14451ec2f125a930f
cahyadi-bs <cahbayu27@gmail.com>
2021-09-09 09:39:08 +0700

    #3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,51 +32,5 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/posts', function () {
-    $blogPost = [
-        [
-            "title" => "Judul Pertama",
-            "slug" => "judul-pertama",
-            "author" => "Cahyadi",
-            "body"=> "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda suscipit et sit repudiandae quae perferendis eius delectus unde placeat? Iste officia totam harum voluptas praesentium quae, explicabo deserunt nulla enim perspiciatis omnis voluptatum atque, maxime aperiam esse quam expedita laboriosam maiores nisi nihil veritatis aspernatur! Animi dolores quis ex voluptates neque cum magnam consequatur ab totam laborum ducimus, odio libero quos, quod amet corrupti quibusdam laudantium nesciunt repellendus quidem fuga sed. Hic iusto alias suscipit delectus quaerat nemo modi."
-        ],
-        [
-            "title" => "Judul Kedua",
-            "slug" => "judul-kedua",
-            "author" => "Donny",
-            "body"=> "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, voluptatem vel! Illo, cupiditate reiciendis. Magni corrupti debitis cumque consectetur quidem?"
-        ],
-    ];
-    return view('posts',[
-        'title' => 'Posts',
-        'posts' => $blogPost
-    ]);
-});
-
-Route::get('posts/{slug}', function ($slug) {
-    $blogPost = [
-        [
-            "title" => "Judul Pertama",
-            "slug" => "judul-pertama",
-            "author" => "Cahyadi",
-            "body"=> "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda suscipit et sit repudiandae quae perferendis eius delectus unde placeat? Iste officia totam harum voluptas praesentium quae, explicabo deserunt nulla enim perspiciatis omnis voluptatum atque, maxime aperiam esse quam expedita laboriosam maiores nisi nihil veritatis aspernatur! Animi dolores quis ex voluptates neque cum magnam consequatur ab totam laborum ducimus, odio libero quos, quod amet corrupti quibusdam laudantium nesciunt repellendus quidem fuga sed. Hic iusto alias suscipit delectus quaerat nemo modi."
-        ],
-        [
-            "title" => "Judul Kedua",
-            "slug" => "judul-kedua",
-            "author" => "Donny",
-            "body"=> "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, voluptatem vel! Illo, cupiditate reiciendis. Magni corrupti debitis cumque consectetur quidem?"
-        ],
-    ];
-
-    $single = [];
-    foreach ($blogPost as $post) {
-        if ($post["slug"] === $slug) {
-            $single = $post;
-        }
-    }
-    return view('post',[
-        'title' => 'Single Post',
-        'post' => $single
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
