2ad77ea39281256b2a7a1e77f761356da9b879a0
cahyadi-bs <cahbayu27@gmail.com>
2021-09-14 09:19:19 +0700

    create factory and implement faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create(); 
-        User::create([
-            'name' => 'Cahyadi',
-            'email' => 'cahyadi@gmail.com',
-            'password' => bcrypt('1234')
-        ]);
+        User::factory(3)->create(); 
+        // User::create([
+        //     'name' => 'Cahyadi',
+        //     'email' => 'cahyadi@gmail.com',
+        //     'password' => bcrypt('1234')
+        // ]);
 
-        User::create([
-            'name' => 'Dody',
-            'email' => 'dody@gmail.com',
-            'password' => bcrypt('1234')
-        ]);
+        // User::create([
+        //     'name' => 'Dody',
+        //     'email' => 'dody@gmail.com',
+        //     'password' => bcrypt('1234')
+        // ]);
         
         Category::create([
             'name' => 'Web Programming',
@@ -39,20 +39,22 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama','category_id' => 3,'slug' => 'judul-pertama','excerpt' => 'Lorem Ipsum Pertama','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 1, 'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Kedua','category_id' => 3,'slug' => 'judul-kedua','excerpt' => 'Lorem Ipsum Kedua','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 1, 'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama','category_id' => 3,'slug' => 'judul-pertama','excerpt' => 'Lorem Ipsum Pertama','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 1, 'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga','category_id' => 3,'slug' => 'judul-ketiga','excerpt' => 'Lorem Ipsum Ketiga','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 2, 'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua','category_id' => 3,'slug' => 'judul-kedua','excerpt' => 'Lorem Ipsum Kedua','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 1, 'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Keempat','category_id' => 3,'slug' => 'judul-keempat','excerpt' => 'Lorem Ipsum Keempat','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 2, 'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga','category_id' => 3,'slug' => 'judul-ketiga','excerpt' => 'Lorem Ipsum Ketiga','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 2, 'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Keempat','category_id' => 3,'slug' => 'judul-keempat','excerpt' => 'Lorem Ipsum Keempat','body' => '<p>ipsum, dolor sit amet consectetur adipisicing elit. Culpa aperiam nostrum fugit sint veniam est odit possimus aliquid deleniti alias placeat iure deserunt accusamus numquam reprehenderit eaque voluptatum eveniet expedita soluta,</p><p>modi porro reiciendis. Est itaque cumque natus quod temporibus ducimus corporis veniam facilis dolorem illum tenetur odio repellat nam eveniet asperiores voluptatibus vitae, at doloribus error quidem quam consectetur provident quia.</p><p>Hic dolorum expedita possimus accusamus eveniet, deleniti laborum saepe libero reiciendis officiis, cupiditate quidem qui deserunt quibusdam accusantium dolor odio, sapiente molestias facere voluptatum reprehenderit eius suscipit. Tempora explicabo nemo earum similique aliquid obcaecati provident a, sequi odio porro cupiditate reprehenderit quibusdam atque quos necessitatibus quo velit modi, nulla enim hic voluptatum aperiam architecto. Aspernatur sapiente natus iure?</p>', 'category_id' => 2, 'user_id' => 2
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 
 
 /*
@@ -36,16 +38,13 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories', function () {
-    return view('categories',[
-        'title' => 'Post Categories',
-        'categories' => Category::all()
+Route::get('/categories', [CategoryController::class , 'index']);
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts',[
+        'title' => 'User Posts',
+        'posts' => $author->posts
     ]);
 });
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category',[
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
-    ]);
-});
+
