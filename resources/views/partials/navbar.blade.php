1ff9fe7cf2003137096be5fcdf429d5df77f8396
cahyadi-bs <cahbayu27@gmail.com>
2021-09-22 10:37:35 +0700

    login&logout | middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,10 +19,29 @@
             <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/about">About</a>
         </li>
         </ul>
+
         <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/login"><i class="bi bi-box-arrow-in-right me-1"></i>Login</a>
+        @auth
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Welcome back, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-window-reverse me-1"></i>My Dashboard</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+                <form action="logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-left me-1"></i>Logout</button>
+                </form>
             </li>
+            </ul>
+        </li>
+        @else 
+        <li class="nav-item">
+            <a class="nav-link" href="/login"><i class="bi bi-box-arrow-in-right me-1"></i>Login</a>
+        </li>
+        @endauth
         </ul>
     </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 
 /*
@@ -41,7 +42,11 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', [CategoryController::class , 'index']);
 
-Route::get('/login', [LoginController::class , 'index']);
+Route::get('/login', [LoginController::class , 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class , 'authenticate']);
+Route::post('/logout', [LoginController::class , 'logout']);
 
-Route::get('/register', [RegisterController::class , 'index']);
+Route::get('/register', [RegisterController::class , 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class , 'store']);
+
+Route::get('/dashboard', [DashboardController::class , 'index'])->middleware('auth');
